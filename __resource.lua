2656a57efedcd09ccd8245f0683e1841c50f39fa
Our Land [QBOCRE] <89742984+daccakareem@users.noreply.github.com>
2021-11-26 02:05:44 +0200

    Add files via upload

---
+++ __resource.lua
@@ -0,0 +1,14 @@
+-- QBCore -- Edited by Nathan#8860
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+
+description 'dacca#6352'
+
+client_scripts {
+	'config.lua',
+	'client/main.lua',
+}
+
+server_scripts {
+	'config.lua',
+	'server/main.lua',
+}
